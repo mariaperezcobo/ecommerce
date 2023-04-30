a50f4a86ec3cf0bb5ced44d97d826cd607403269
Maria Perez Cobo <mariaperezcobo@hotmail.com>
2023-04-30 07:44:30 -0300

    agregue array y metodos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let entrada = Number(prompt ("Ingrese el número del articulo a comprar. \n 1. Calza Lisboa \n 2. Calza Londres \n 3. Calza Madrid \n 4. No quiero comprar ningun producto"));
+/*let entrada = Number(prompt ("Ingrese el número del articulo a comprar. \n 1. Calza Lisboa \n 2. Calza Londres \n 3. Calza Madrid \n 4. No quiero comprar ningun producto"));
 
 
 let precioLisboa= 18000;
@@ -9,6 +9,7 @@ let nuevoPrecio;
 let nuevoPrecioDos;
 let cuotas;
 let totalCompra=0;
+const carrito2 =[];
 
 if(entrada == 1){
     alert ("Ha seleccionado la calza Lisboa. Precio "+ `${precioLisboa}`);
@@ -23,8 +24,8 @@ if(entrada == 1){
     alert ("No seleccionó ningun producto");
     nuevoPrecio=0;
 };
-
-let entradaDos = Number(prompt ("¿quiere agregar otro articulo al carrito? Ingrese el número del articulo a comprar. \n 1. Calza Lisboa \n 2. Calza Londres \n 3. Calza Madrid \n 4. No quiero comprar ningun producto"));
+*/
+/*let entradaDos = Number(prompt ("¿quiere agregar otro articulo al carrito? Ingrese el número del articulo a comprar. \n 1. Calza Lisboa \n 2. Calza Londres \n 3. Calza Madrid \n 4. No quiero comprar ningun producto"));
 
 if(entradaDos == 1){
     alert ("Ha seleccionado la calza Lisboa. Precio "+ `${precioLisboa}`);
@@ -74,11 +75,11 @@ if(entrada <4 && entrada >=1){
 
 
 
-
+*/
 
 //OBJETOS
 
-function Producto (nombre, precio){
+/*function Producto (nombre, precio){
     this.nombre = nombre;
     this.precio = precio;
 }
@@ -94,12 +95,12 @@ console.log(producto2);
 console.log(producto3);
 
 
-
+*/
 
 //ARRAY
 const productos = [
-    "Calzas Lisboa",
-    "Calzas Londres",
+    {nombre:"Calzas Lisboa", precio: 15000},
+    {nombre: "Calzas Londres", precio: 12000}
 ] 
 console.log(productos.join(","));
 //para recorrer el array
@@ -108,15 +109,144 @@ for (let i=0; i< productos.length; i++){
 }
 
 //para agregar productos al array
-productos.push("Calzas Madrid");
-productos.unshift("Top Miami");
+productos.push(
+    {nombre:"Calzas Madrid", precio:10000}); 
+console.log(productos);
+
+
+//let entrada = Number(prompt ("Ingrese el número del articulo a comprar. \n 1. Calza Lisboa \n 2. Calza Londres \n 3. Calza Madrid \n 4. No quiero comprar ningun producto"));
+
+let resultado3= productos.filter((producto) => producto.nombre ==="Calzas Lisboa");
+console.log(resultado3);
+
+let compra= productos.filter((producto) => producto.nombre ==="Calzas Madrid");
+console.log(compra);
+let totalCarrito2 = compra.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+console.log(totalCarrito2);
+
+const carrito=[];
+let totalcompra;
+
+
+//carrito.push(compra);
+//carrito.push(resultado3);
+console.log(carrito);
+
+const carritoFinal2 = compra.concat(resultado3);
+console.log(productos);
+console.log(carritoFinal2);
+
+const carritoFinal4 = carritoFinal2.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+console.log(carritoFinal4);
+
+const totalCarrito = carrito.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+console.log(totalCarrito);
+
+
+productos.forEach ((producto)=> {
+    console.log(producto.precio);
+});
+/*
+let productoElegido = prompt("ingrese el producto");
+console.log (producto.find((producto) => producto.nombre === productoElegido)); 
+*/
+
+/*let resultado3 = productos.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+console.log(resultado3);
+
+/*productos.maps((f,i)=>{
+    console.log(`${i}:${f}`)
+})*/
+
+/*trae el primer valor q encuentra
+let resultado3 = productos.find((producto)=> producto.precio >1500);
+console.log(resultado3);*/
+
+
+
+let resultado4 = productos.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+console.log(resultado4);
+
+let precioProductoUno = productos.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+console.log(precioProductoUno);
+
+
+
 
 //unir dos array
-const productos2 = ["Remera Barcelona"];
+/*const productos2 = ["Remera Barcelona"];
 const productosFinal = productos.concat(productos2);
 console.log(productos);
-console.log(productosFinal);
+console.log(productosFinal);*/
 
-console.log(productos.indexOf("Top Miami"));
+/*productos.indexOf(
+{nombre:"Top Miami", precio: 7000});
+console.log(productos);
+*/
 
+let entrada = Number(prompt ("Ingrese el número del articulo a comprar. \n 1. Calza Lisboa \n 2. Calza Londres \n 3. Calza Madrid \n 4. No quiero comprar ningun producto"));
 
+let nuevoPrecio;
+let nuevoPrecioDos;
+let cuotas;
+let totalCompra=0;
+const carrito2 =[];
+let primerCarrito;
+let precioPrimerCarrito;
+let precioCarritoLisboa;
+let precioCarritoMadrid;
+let precioCarritoLondres;
+let carritoLisboa;
+let carritoMadrid;
+let carritoLondres;
+
+if(entrada == 1){
+    carritoLisboa = productos.filter((producto) => producto.nombre ==="Calzas Lisboa");
+    console.log(carritoLisboa);
+    carrito.push(carritoLisboa);
+    precioCarritoLisboa = carritoLisboa.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+    console.log(precioCarritoLisboa);
+    alert ("Ha seleccionado la calza Lisboa. Precio "+ `${precioCarritoLisboa}`);
+} else if (entrada == 2) {
+    carritoLondres = productos.filter((producto) => producto.nombre ==="Calzas Londres");
+    console.log(carritoLondres);
+    carrito.push(carritoLondres);
+    precioCarritoLondres = carritoLondres.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+    console.log(precioCarritoLondres);
+    alert ("Ha seleccionado la calza Londres. Precio "+ `${precioCarritoLondres}`);
+} else if (entrada == 3) {
+    carritoMadrid = productos.filter((producto) => producto.nombre ==="Calzas Madrid");
+    console.log(carritoMadrid);
+    carrito.push(carritoMadrid);
+    precioCarritoMadrid = carritoMadrid.reduce ((acumulado, producto) => {
+    return acumulado + producto.precio},0);
+    console.log(precioCarritoMadrid);
+    alert ("Ha seleccionado la calza Madrid. Precio "+ `${precioCarritoMadrid}`);
+} else if (entrada >= 4 || entrada == "" ) {
+    alert ("No seleccionó ningun producto");
+    nuevoPrecio=0;
+};
+
+/*let entradaDos = Number(prompt ("¿quiere agregar otro articulo al carrito? Ingrese el número del articulo a comprar. \n 1. Calza Lisboa \n 2. Calza Londres \n 3. Calza Madrid \n 4. No quiero comprar ningun producto"));
+
+if(entradaDos == 1){
+    alert ("Ha seleccionado la calza Lisboa. Precio "+ `${precioLisboa}`);
+    nuevoPrecioDos = `${precioLisboa}`;
+} else if (entradaDos == 2) {
+        alert ("Ha seleccionado la calza Londres. Precio "+ `${precioLondres}`);
+        nuevoPrecioDos = `${precioLondres}`;
+} else if (entradaDos == 3) {
+    alert ("Ha seleccionado la calza Madrid. Precio "+ `${precioMadrid}`);
+    nuevoPrecioDos = `${precioMadrid}`;
+} else if (entradaDos >= 4 || entradaDos == "" ) {
+    alert ("No seleccionó ningun producto");
+    nuevoPrecioDos=0;
+};*/
